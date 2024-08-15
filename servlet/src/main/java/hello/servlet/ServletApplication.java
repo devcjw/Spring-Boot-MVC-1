337201acd675274366ba37710472859aa3a0b230
devcjw <devcjw@gmail.com>
2024-08-15 15:26:36 +0900

    Section 5

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,11 +3,10 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ServletApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ServletApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ServletApplication.class, args);
+    }
 }
